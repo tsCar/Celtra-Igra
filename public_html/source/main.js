2e690da20b9447939b5c704a6b19dba20df1e015
Zla Patka <Zla Patka@ZlaPatka.fritz.box>
2017-05-11 21:45:12 +0200

    djelic podloge, los skok

--- public_html/source/main.js
+++ public_html/source/main.js
@@ -1,66 +1,61 @@
 window.onload = function() {
     //screen.orientation.lock('portrait');
-
     let canvas = document.getElementById("canvas");
+    let gumb=document.getElementById("jump");
     let ctx = canvas.getContext("2d");
-    canvas.width  = window.innerWidth;
-    canvas.height = window.innerHeight;
-    let tekst=  document.getElementById("tu");
+    let width = canvas.width  = screen.width;
+    let height = canvas.height = screen.height;
 
-    let width = canvas.width ;
-    let height = canvas.height;
+    let recty= height-40;
+    let rectx=50;
+    let rectdy=20;
+    let rectdx=200;
 
-    var x = canvas.width/2;
-    var y = canvas.height-60;
-    var dx = 0;
-    var dy = 0;
-    let count=0;
-    let posy = null;
-
-    //load tilesets
-    let background = new Image();
-    background.src = "slicice/pozadina.jpg";
-    ctx.drawImage(background, 0, 0, background.width, background.height, 0, 0, width, height);
-
-//    window.addEventListener('devicemotion', function(event) {
-//        alert(event.accelerationIncludingGravity.x)
-
-//    }, false);
+    var ballx = 150;
+    var bally = 20;
+    var balldx = 0;
+    var balldy = 0;
 
     window.ondevicemotion = function(event) {  
-        let nagib = Math.round(event.accelerationIncludingGravity.y*10) / 10;
-        if(nagib>1)
-            dx=2;
-        else if(nagib<-1)
-            dx=-2;
-        else dx=0;
-  
+            let nagib = Math.round(event.accelerationIncludingGravity.y*10) / 10;
+            if(nagib>1)
+                balldx=2;
+            else if(nagib<-1)
+                balldx=-2;
+            else balldx=0;  
+        };
+    gumb.ontouchstart=function(){
+        for (i=60;i>0;i--){
+            bally-=i;
+            draw();
+        }
     };
-        update();
+    update();
 
     function update() {
-//        ctx.drawImage(background, 0, 0, background.width, background.height, 0, 0, width, height);
-        ctx.clearRect(0,0, width, height);
-        ctx.drawImage(background, 0, 0, background.width, background.height, 0, 0, width, height);
-
+        ctx.clearRect(0, 0, width,height);
+        ctx.rect(rectx,recty,rectdx,rectdy);
+        ctx.fill();
+        if (naPodlozi())balldy=0;
+        else balldy=1;
         function drawBall() {
             ctx.beginPath();
-            ctx.arc(x, y, 10, 0, Math.PI*2);
+            ctx.arc(ballx, bally, 10, 0, Math.PI*2);
             ctx.fillStyle = "#0095DD";
             ctx.fill();
             ctx.closePath();
-
-//            ctx.fillText(count,x,y);
-//            count++;
         }
-
         function draw() {
             drawBall();
-            x += dx;
-            y += dy;
+            if (ballx+balldx>0 && ballx+balldx< canvas.width) ballx += balldx;
+            bally += balldy;
         }
-
+        function naPodlozi(){
+            if (bally===recty && ballx>rectx && ballx<rectx+rectdx) return true;
+            else return false;
+        }
         draw();
         requestAnimationFrame(update);
     }
+    
 };
